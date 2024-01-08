618a406671fb48a83ee8aec63ad59fc887372397
Bunk0vsky <jakub.luczynski.work@gmail.com>
2024-01-08 15:11:53 +0100

    Updating new recipes added

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const KEY = 'abf40b9f-1914-449c-b17f-082bddabbacb';
+export const MODAL_CLOSE_SEC = 2.5;
